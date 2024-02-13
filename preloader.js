7e092815d626e9d7896471140bc3dd42225ff549
Kashinka <azalena@yandex.ru>
2024-02-13 20:41:21 +0300

    Added addEventListener

--- preloader.js
+++ preloader.js
@@ -1,4 +1,4 @@
-window.onload = function(){
+document.addEventListener("DOMContentLoaded", ready) = function(){
     let preloader = document.getElementById('preload');
     preloader.style.display = 'none';
 };
